638483d36495e2e11b6d0312b616131e6f076f85
Nikita Bystritskiy <spr1ng.h4des@yandex.ru>
2022-11-13 16:41:14 +0300

    Add wezterm config.

---
+++ .wezterm.lua
@@ -0,0 +1,11 @@
+local wezterm = require 'wezterm'
+return {
+  font = wezterm.font('JetBrains Mono', { weight = 'Bold' }),
+  weight = 'Bold',
+  font_size = 13,
+  harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
+  color_scheme = 'Rosé Pine (base16)',
+  window_decorations = 'RESIZE', 
+  hide_tab_bar_if_only_one_tab = true,
+  adjust_window_size_when_changing_font_size = false, 
+}
