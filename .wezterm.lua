b12c37ad05cf3dff83f90762caa7bbaf83a3705d
Nikita Bystritskiy <spr1ng.h4des@yandex.ru>
2022-11-17 04:59:23 +0300

    Update .wezterm.lua

--- .wezterm.lua
+++ .wezterm.lua
@@ -4,7 +4,7 @@ return {
   weight = 'Bold',
   font_size = 14,
   harfbuzz_features = { 'calt=0', 'clig=0', 'liga=0' },
-  color_scheme = 'Gruvbox Light',
+  color_scheme = 'Rosé Pine (base16)',
   window_decorations = 'RESIZE', 
   hide_tab_bar_if_only_one_tab = true,
   adjust_window_size_when_changing_font_size = false, 
